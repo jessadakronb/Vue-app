19fc837b3eb3038cd72703e9980d931b2e47c179
Jessadakron <59275824+jessadakronb@users.noreply.github.com>
2021-07-22 15:34:15 +0700

    Initial commit

---
+++ src/pages/main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <manufacturer v-slot="data">
+      <devices :manufacturerId="data.factId"></devices>
+    </manufacturer>
+  </div>
+</template>
+
+
+<script>
+import Devices from "@/components/Devices.vue";
+import Manufacturer from "@/components/Manufacturer.vue";
+export default {
+  name: "App",
+  components: {
+    Manufacturer: Manufacturer,
+    Devices: Devices,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
